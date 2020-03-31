845d4a2b48d23d264f93b96d5a6a99725f758e1a
bdsqqq <igorbedesqui@gmail.com>
2020-03-31 16:03:50 -0300

    webfont implementation

--- src/index.js
+++ src/index.js
@@ -1,8 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import WebFont from 'webfontloader';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+
+WebFont.load({
+  google: {
+    families: ['Montserrat:400,700', 'sans-serif']
+  }
+});
 
 ReactDOM.render(
   <React.StrictMode>
